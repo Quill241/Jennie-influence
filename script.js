e51bc3e40f6f6ca7852a30f2ed2a72cf6e3f6bf2
Quill241 <happinessolawunmi@gmail.com>
2025-08-18 20:47:26 +0100

    add script.js file

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// const hamburger = document.getElementById('hamburger');
+// const sideNav = document.getElementById('sideNav');
+
+// hamburger.addEventListener('click', () => {
+//   sideNav.classList.toggle('active');
+// });
+
+    
+        // function toggleMute() {
+        //     var audio = document.getElementById('background-music');
+        //     var muteButton = document.getElementById('mute-button');
+
+        //     if (audio.muted) {
+        //         audio.muted = false;
+        //         muteButton.textContent = 'Mute'; // Change button text
+        //     }else {
+        //         audio.muted = true;
+        //         muteButton.textContent = 'Unmute'; // Change button text
+        //     }
+        // }
+    
